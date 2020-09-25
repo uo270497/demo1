96bc80d7d4624012eff3786e8aff7b1febf2da86
Usuario <Usuario@DESKTOP-JJR5FO5>
2020-09-26 00:15:01 +0200

    cambio y borrado

---
+++ src/demo/Demo2.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Demo2 {
+	
+	public static void main(String args[]) {
+		System.out.println("linea 1");
+		System.out.println("linea 2");
+		System.out.println("linea 3");
+	}
+
+}
